44dbf41fa4e4a333cd71caf5a2d23b1c70ae64bf
Radu Aldea <aldearadu@gmail.com>
2026-04-24 09:02:07 +0100

    chore: commit lint cleanup artifacts

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { ProfileHeader } from "@/components/profile/profile-header";
 import { ActivityList } from "@/components/profile/activity-list";
